4005fc4ff2f748b9a446284eecfb3a0cfc2a0789
Eva Panizo <panizo.eva@gmail.com>
2019-01-27 15:51:36 +0100

    Add Advanced MySQL lab - Challenge 1 completed (missing challenge 2 and 3)

---
+++ 04.-Advanced-MySQL/your-code/solutions.sql
@@ -0,0 +1,39 @@
+# Database
+USE publications;
+
+# Challenge 1 - Most Profiting Authors
+## Step 1
+CREATE TEMPORARY TABLE publications.royalties_per_sale
+SELECT ta.au_id AS AuthorID, s.title_id AS TitleID,
+(t.price * s.qty) * (t.royalty / 100) * (ta.royaltyper / 100) AS RoyaltyPerSale
+FROM publications.sales s
+INNER JOIN publications.titles t ON s.title_id = t.title_id
+INNER JOIN publications.titleauthor ta ON t.title_id = ta.title_id;
+
+## Step 2
+CREATE TEMPORARY TABLE publications.royalties_per_title_and_author
+SELECT AuthorID, TitleID, SUM(RoyaltyPerSale) AS RoyaltyPerTitleAndAuthor
+FROM publications.royalties_per_sale
+GROUP BY TitleID, AuthorID;
+
+## Step 3
+CREATE TEMPORARY TABLE publications.advance_per_author
+SELECT ta.au_id AS AuthorID, SUM(t.advance * (ta.royaltyper / 100)) AS TotalAdvance
+FROM publications.titles t
+INNER JOIN publications.titleauthor ta ON t.title_id = ta.title_id
+GROUP BY AuthorID;
+
+CREATE TEMPORARY TABLE publications.royalty_per_author
+SELECT AuthorID, SUM(RoyaltyPerTitleAndAuthor) AS TotalRoyalty
+FROM publications.royalties_per_title_and_author
+GROUP BY AuthorID;
+
+SELECT apa.AuthorID, ROUND(TotalRoyalty + TotalAdvance,2) AS Profit
+FROM royalty_per_author rpa
+INNER JOIN publications.advance_per_author apa ON apa.AuthorID = rpa.AuthorID
+ORDER BY Profit DESC
+LIMIT 3;
+
+# Challenge 2 - Alternative Solution
+
+# Challenge 3 - Create A Permanent Table Of The Most Profiting Authors
