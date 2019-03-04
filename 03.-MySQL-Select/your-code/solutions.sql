9ab82c1f5d3c8c76114d87ca862a3a90473b36b8
Eva Panizo <9594137+evapanizo@users.noreply.github.com>
2019-03-04 15:49:34 +0100

    spaces OCD

--- 03.-MySQL-Select/your-code/solutions.sql
+++ 03.-MySQL-Select/your-code/solutions.sql
@@ -28,7 +28,7 @@ ORDER BY Total DESC, AuthorID DESC
 LIMIT 3;
 
 # Challenge 4
-SELECT a.au_id AS AuthorID, a.au_lname AS LastName, a.au_fname AS FirstName, IFNULL(SUM(s.qty),0)  AS Total
+SELECT a.au_id AS AuthorID, a.au_lname AS LastName, a.au_fname AS FirstName, IFNULL(SUM(s.qty),0) AS Total
 FROM publications.authors a
 LEFT JOIN publications.titleauthor ta ON ta.au_id = a.au_id
 LEFT JOIN publications.sales s ON s.title_id = ta.title_id
